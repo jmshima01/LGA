ba7856837408ee2584138c47205c883978c4b67d
ChandlerSeamount <118400524+ChandlerSeamount@users.noreply.github.com>
2024-02-23 00:11:16 -0700

    Update main.cpp

--- C++/main.cpp
+++ C++/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 using namespace std;
-
+// Structure that just stores the non-terminal as a string and the options it can be reduced to as a vector.
 struct rule {
     string nonT;
     vector<string> result;
@@ -15,18 +15,18 @@ int main(int argc, char *argv[]) {
     ifstream file_in;
     file_in.open(argv[1]);
 
-    vector<rule> rules;
-    set<string> notTs;
-    set<string> symbols;
-    string last;
-    string nonT;
-    string start;
+    vector<rule> rules; // The rules in order
+    set<string> notTs; // Contains all the non terminals
+    set<string> symbols; // Contains all the terminals and non terminals
+    string last; // The last value read in
+    string nonT; // The last non terminal read in
+    string start; // The starting terminal
     file_in >> last;
-    
+    // Reads in the grammer
     while (!file_in.eof()) {
         string arrow;
         file_in >> arrow;
-        if (arrow == "->") {
+        if (arrow == "->") { // if the value is an arrow then the last value is a non terminal so it adds it to the list and changes the vales
             notTs.insert(last);
             nonT = last;
             if (rules.size() > 0) {
@@ -37,21 +37,21 @@ int main(int argc, char *argv[]) {
             current_rule.nonT = nonT;
             rules.push_back(current_rule);
             symbols.insert(last);
-        } else if (arrow == "|") {
+        } else if (arrow == "|") { // If it is the | then its the end of the current rule but doesn't change the non terminal
             rule current_rule;
             current_rule.nonT = nonT;
             rules.push_back(current_rule);
-        } else if (arrow == "$") {
+        } else if (arrow == "$") { // This means that this rule is the starting rule so set it as such
             rules.back().result.push_back(arrow);
             start = nonT;
-        } else {
+        } else { // Its just some random value
             
             rules.back().result.push_back(arrow);
             symbols.insert(arrow);
         }
         last = arrow;
     }
-
+    // Print out the values
     cout << "Grammar Non-Terminals" << endl;
     bool first = true;
     for (auto a:notTs) {
